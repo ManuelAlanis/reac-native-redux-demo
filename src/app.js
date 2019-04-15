93d983c8c61e04684985113160029a9cc87c81f6
Manuel Alanis <alanism@uabc.edu.mx>
2019-04-15 13:24:21 -0700

    connect redux reducer with listComponent

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import Reducers from './reducers';
+import SuperheroesList from './superheroesList';
 
 const App = () => (
     <Provider store={createStore(Reducers)}>
-        <View />
+        <View>
+            <SuperheroesList />
+        </View>
     </Provider>
 );
 
---
+++ src/superheroesList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+class SuperheroesList extends Component {
+    getSuperheroes() {
+        const { superheroes } = this.props;
+        console.log('superheroes', superheroes);
+        const superheroesData = superheroes.map((heroes, key) => 
+            <Text key={key}>{heroes.superhero}</Text>);
+        return superheroesData;
+    }
+    render() {
+        console.log(this.props);
+        return (
+            <View>
+                {this.getSuperheroes()}
+            </View>
+        );
+    }
+}
+
+const mapStateToProps = state => ({ superheroes: state.superheroes });
+
+export default connect(mapStateToProps)(SuperheroesList);
